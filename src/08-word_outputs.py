dc174a4f8bcf44b5a09f71838cc5303a93812b56
Daniel Chen <Maveri2k@yahoo.com>
2017-04-17 13:33:37 -0400

    output files

---
+++ src/08-word_outputs.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+df = pd.read_csv('output/word_sentiments.csv')
+
+print(pd.crosstab(df.swn_pos, df.pos_neg, margins=True))
+
+# write out 2x2 table values
+# pos_n pos_v neg_n neg_v
+df.ix[(df.pos_neg == 'neg') & (df.swn_pos == 'n'), 'word'].to_csv('output/words_neg_n.csv', index=False)
+df.ix[(df.pos_neg == 'neg') & (df.swn_pos == 'v'), 'word'].to_csv('output/words_neg_v.csv', index=False)
+df.ix[(df.pos_neg == 'pos') & (df.swn_pos == 'n'), 'word'].to_csv('output/words_pos_n.csv', index=False)
+df.ix[(df.pos_neg == 'pos') & (df.swn_pos == 'v'), 'word'].to_csv('output/words_pos_v.csv', index=False)
+
+all_words = pd.read_csv('all_word_counts.csv', header=None, names=['word', 'count'])
+nv_words = pd.read_csv('noun_verbs.csv').drop_duplicates()
+
+word_counts = all_words.merge(nv_words, on='word')
+combined = df.merge(word_counts, on='word')
+
+counts = combined[['word', 'swn_pos', 'pos_neg', 'count']]
+
+counts.to_csv('output/word_counts_pos_sent.csv')
